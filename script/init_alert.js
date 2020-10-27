b9cf18a74fb563fcc954064d4dd244eab6fa55eb
Manuel <manuelh0107g@gmail.com>
2020-10-27 16:32:41 -0500

    Change Var code

--- script/init_alert.js
+++ script/init_alert.js
@@ -16,3 +16,38 @@
 	}
 	document.getElementById("ejemplo").innerHTML = mensaje;
 }*/
+
+////////////////////////////////////Security//////////////////////////////////
+  var user = "Manu";
+  var user2 = "Mayra";
+  var user3 = "Efrain";
+  var pass = "pv5260";
+ 
+
+
+  alert("Sistema de reportes de gastos administrativos ")
+
+  var op = prompt("Ingrese usuario administrativo");
+  
+
+  if(op === user || op === user2 || op === user3){
+    var utr = prompt("Ingrese su contraseña")
+
+    if(utr === pass){
+      alert("Bienvenido " + op + "\n"+
+      	"Su numero identificador es 25TgTT89A")
+
+     
+    }else{
+      alert("Constraseña incorrecta")
+      location.href="https://www.folklore-panama.online"
+    }
+
+
+  }else{
+    alert("Acceso denegado")
+    location.href="http://www.google.com";
+    alert("Redireccionando a Google.com");
+  }
+
+  document.getElementById('op').innerHTML = op;
